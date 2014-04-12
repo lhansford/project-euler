955f42eaa36aa182de2c1a350c9d3ec32344f522
Luke Hansford <l.s.hansford@gmail.com>
2014-04-12 14:54:55 +1000

    Completed problems 31 & 57

--- python/problem_31.py
+++ python/problem_31.py
@@ -1,2 +1,14 @@
-coins = [1,2,5,10,100]
+from itertools import combinations_with_replacement
 
+def get_combos(total):
+	#This was adapted from 
+	#http://www.mathblog.dk/project-euler-31-combinations-english-currency-denominations/
+	coins = [1, 2, 5, 10, 20, 50, 100, 200]
+	ways = [0 for i in xrange(0, total+1)]
+	ways[0] = 1
+	for x in xrange(0,len(coins)):
+		for y in xrange(coins[x], total+1):
+			ways[y] += ways[y - coins[x]]
+	return ways[-1]
+
+print get_combos(200)
--- python/problem_57.py
+++ python/problem_57.py
@@ -1,2 +1,20 @@
-def x():
-	return 1 + 
+from __future__ import division
+from fractions import Fraction
+
+def create_fraction(level):
+	fraction = 2 + Fraction(1,2)
+	for x in xrange(level, 1, -1):
+		fraction = 1/fraction
+		fraction += 2
+	return fraction-1
+
+def count_longer_numerators(limit):
+	count = 0
+	for x in xrange(1,limit+1):
+		f = create_fraction(x)
+		print f
+		if len(str(f.numerator)) > len(str(f.denominator)):
+			count += 1
+	return count
+
+print count_longer_numerators(1000)
