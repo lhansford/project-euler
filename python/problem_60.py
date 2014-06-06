919f9b60bc1080b99fd5c01f1ee1132aced36bc8
Luke Hansford <l.s.hansford@gmail.com>
2014-06-06 15:09:35 +1000

    Problem 60 complete in Python

--- python/problem_60.py
+++ python/problem_60.py
@@ -1,4 +1,7 @@
+from collections import OrderedDict
 from itertools import combinations
+
+## THIS IS A LITTLE SLOW, BUT IT WORKS (takes 2-3mins to get answer)
 
 def is_prime(number):
 	if number > 1:
@@ -21,74 +24,88 @@ def prime_gen(num=2):
 def get_all_primes(start,limit):
 	primes = []
 	p_gen = prime_gen(start)
-	p = p_gen.next()
+	p = next(p_gen)
 	while p < limit:
 		primes.append(p)
-		p = p_gen.next()
+		p = next(p_gen)
 	return primes
 
-def can_split(n):
-	str_num = str(n)
-	pairs = []
-	for x in xrange(1, len(str_num)):
-		if is_prime(int(str_num[:x])) and is_prime(int(str_num[x:])):
-			pairs.append((int(str_num[:x]),int(str_num[x:])))
-	return pairs
+def get_tuples_with_n_occurences(pairs, n):
+	pairs_with_n_occurences = []
+	for pair in pairs:
+		count = 0
+		for pair2 in pairs:
+			if pair[0] == pair2[0] or pair[0] == pair2[1]:
+				count += 1
+			if count == n:
+				pairs_with_n_occurences.append(pair)
+				break
+	return pairs_with_n_occurences
 
-def get_matching_pairs(pairs):
-	matches = []
-	while len(pairs) > 0:
-		pair = pairs.pop()
-		for p in pairs:
-			if concatenates(pair, p):
-				matches.append((pair,p))
-	return matches
+def get_prime_sets(pairs, n):
+	pair_dict = {}
+	
+	for pair in pairs:
+		if pair_dict.get(pair[0]):
+			pair_dict[pair[0]].append(pair[1])
+		else:
+			pair_dict[pair[0]] = [pair[1]]
+	pair_dict = OrderedDict(sorted(pair_dict.items(), key=lambda t: t[0]))
 
-def get_groups_of_5(matching_pairs):
-	groups_of_5 = []
-	while len(matching_pairs) > 0:
-		mp1 = matching_pairs.pop()
-		for mp2 in matching_pairs:
-			if concatenates(mp1[0], mp2[0]) and concatenates(mp1[1], mp2[0]) and concatenates(mp1[0], mp2[1]) and concatenates(mp1[1], mp2[1]):
-				groups_of_5.append((mp1,mp2))
-	return groups_of_5
+	prime_sets = []
 
-# def concatenates(pair1, pair2):
-# 	if not is_prime(int(str(pair1[0])+str(pair2[0]))):
-# 		return False
-# 	if not is_prime(int(str(pair1[1])+str(pair2[0]))):
-# 		return False
-# 	if not is_prime(int(str(pair1[0])+str(pair2[1]))):
-# 		return False
-# 	if not is_prime(int(str(pair1[1])+str(pair2[1]))):
-# 		return False
-# 	return True
+	for key in pair_dict.keys():
+		for item in pair_dict[key]:
+			count = 2
+			p_set = [key,item]
+			for item2 in pair_dict.get(item,[]):
+				if item2 in pair_dict[key]:
+					count+=1
+					p_set.append(item2)
+				if count == n:
+					prime_sets.append(p_set)
+					break
+			if count < n:
+				for item2 in pair_dict.get(key,[]):
+					if item in pair_dict.get(item2,[]):
+						count+=1
+						p_set.append(item2)
+					if count == n:
+						prime_sets.append(p_set)
+						break
+	print(prime_sets)
+	return(prime_sets)
 
 def find_concatenating_primes(n, test_range):
-	candidates = [[p] for p in get_all_primes(2, test_range)]
-	for x in xrange(0,n):
-		next = []
-		for c in candidates:
-			for p in get_all_primes(2, test_range):
-				if concatenates(c, p):
-					next.append(c[:]+[p])
-		candidates = next[:]
-		print candidates
-	return candidates
+	candidates = get_all_primes(2, test_range)
+	pairs = set()
+	for a in candidates:
+		for b in get_all_primes(2, test_range):
+			if concatenates(a, b):
+				pairs.add(tuple(sorted([a,b])))
+	pairs = get_tuples_with_n_occurences(pairs, n)
+	return get_prime_sets(pairs, n)
 
 
-def concatenates(current, new):
-	for n in current:
-		if not is_prime(int(str(n)+str(new))):
-			return False
-		if not is_prime(int(str(new)+str(n))):
-			return False
-	return True
+def concatenates(a, b):
+	if is_prime(int(str(a)+str(b))) and is_prime(int(str(b)+str(a))):
+		return True
+	return False
 
-print find_concatenating_primes(5, 10000)
+def get_min_sum(list):
+	return min([sum(x) for x in list])
 
-# all_pairs = []
-# primes = get_all_primes(2,50000)
-# for p in primes:
-# 	all_pairs+=can_split(p)
-# print get_groups_of_5(get_matching_pairs(all_pairs))
+def remove_non_concatenating(list):
+	valid_sets = []
+	for sublist in list:
+		valid = True
+		for combo in combinations(sublist,2):
+			if not concatenates(combo[0], combo[1]):
+				valid = False
+				break
+		if valid:
+			valid_sets.append(sublist)
+	return valid_sets
+
+c = find_concatenating_primes(5, 10000)
+print(get_min_sum(remove_non_concatenating(c)))
