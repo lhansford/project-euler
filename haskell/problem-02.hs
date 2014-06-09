50a3af8d68b15ae2cdba96a506c380394783905b
Luke Hansford <l.s.hansford@gmail.com>
2014-06-10 00:59:03 +1000

    Completed problem 2 in Haskell

--- haskell/problem-02.hs
+++ haskell/problem-02.hs
@@ -1,9 +1,24 @@
-fibonacci :: Integer -> Integer
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+--Takes a list of integers (supposedly a fibonacci sequence) and adds the next
+--integer in the sequence to the front of the list
+nextFibonacci :: [Int] -> [Int]
+nextFibonacci [] = [1]
+nextFibonacci [1] = [2,1]
+nextFibonacci (x:y:xs) = x+y:x:y:xs
 
-getFibonaccis :: Integer -> [a]
-getFibonaccis limit = [f1+f2 | f]
+--Takes an empty list and limit and returns a list of all fibonnaci numbers
+--with a value less than limit
+getFibonaccis :: [Int] -> Int -> [Int]
+getFibonaccis [] limit = getFibonaccis (nextFibonacci []) limit
+getFibonaccis nums limit
+	| head nums < limit = getFibonaccis (nextFibonacci nums) limit
+	| otherwise = nums
 
-sumEvens nums = sum [x | x <- nums, mod x 2 == 0]
+--Returns the sum of all even numbers in a given list of integers
+sumEvens :: [Int] -> Int
+sumEvens [] = 0
+sumEvens (x:xs)
+	| x `mod` 2 == 0 = x + sumEvens xs
+	| otherwise = 0 + sumEvens xs
+
+--Run the following to get the answer
+--sumEvens (getFibonaccis [] 40000000)
